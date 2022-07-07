a10018dff9d80a0f75d532e1ce6abe829dd65004
danhlucifer <danhit3003@gmail.com>
2022-07-07 16:40:09 +0700

    Signup Page

--- front-end/src/Components/Signup/Signup.jsx
+++ front-end/src/Components/Signup/Signup.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import images from '../../constants/images';
+import { Link } from 'react-router-dom';
 import './Signup.css';
 
 function Signup() {
@@ -12,6 +13,51 @@ function Signup() {
         <div className='signup-title'>
           <div className='signup-title-sub'>
             <img src={images.subSignup} alt="" style={{ width: 300, height: 600 }}/>
+          </div>
+
+          <div className='signup-title-input'>
+            <div className='signup-title-input-header'>
+              <h1>Sign Up</h1>
+              <div className='signup-title-input-header-img' style={{width: 80,height:80}}>
+                <img src={images.userIcon} alt="" />
+                
+              </div>
+              <div className='img-plus'><i class="fas fa-arrow-alt-down"></i></div>
+            </div>
+            <div className='signup-title-input-container'>
+              <form action="" className='form'>
+                <div className='form-input'>
+                  <span className='icon'><i class="fal fa-user"></i></span>
+                  <input type="text" name='username' placeholder='User Name'/>
+                </div>
+              </form>
+              <form action="" className='form'>
+                <div className='form-input'>
+                  <span className='icon'><i class="far fa-envelope"></i></span>
+                  <input type="email" name='email' placeholder='Email' />
+                </div>
+              </form>
+              <form action="" className='form'>
+                <div className='form-input'>
+                  <span className='icon'><i class="far fa-lock"></i></span>
+                  <input type="password" name='password' placeholder='Password'/>
+                </div>
+              </form>
+              <form action="" className='form'>
+                <div className='form-input'>
+                  <span className='icon'><i class="far fa-lock"></i></span>
+                  <input type="password" name='password' placeholder='Confirm Password'/>
+                </div>
+              </form>
+            </div>
+            <div className='signup-title-input-footer'>
+              <div className='signup-title-input-footer-button'>
+                <button>Register</button>
+              </div>
+              <div className='signup-title-input-footer-text'>
+                <p>Already have an account ? <Link to='/login'style={{textDecoration: 'none'}}><span>Login</span></Link></p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
